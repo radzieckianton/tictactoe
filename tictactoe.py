601e20cedb8dc05983aff84e60d4e339910ea277
Anton Savitski <antonsavitski17@gmail.com>
2019-10-08 22:37:29 +0200

    new commit test

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,5 @@
 import random
+import sys
 """This is the simple well-known game. Tic tac toe. First you will be asked to pick a number from 1 to 20, and if your number will be greater than computer's number you will start the game. 'X' always starts. You are choosing the field on the board by entering number from 1 to 9."""
 
 def printBoard(board):
